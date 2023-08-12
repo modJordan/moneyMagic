738ddf2f2426dc94ee01b0407122665a17f28762
modjordan <mod.jordan@icloud.com>
2023-08-12 12:26:22 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Dollar from './money';
+
+//Business Logic
+
+function getMoney(amount) {
+  Dollar.getMoney(amount)
+    .then(function (response) {
+      if (response.result === success) {
+        printNewMoney(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
+
+//UI Logic 
+
+
+function printNewMoney(response) {
+  const newMoney = document.querySelector("#result");
+  response.conversion_rates.forEach(response => {
+    newMoney.innerHTML = `${response.conversion_rates[0]}`;
+  });
+
+
+}
+
+function printError(error) {
+  document.getElementById("result").innerText = `There was an error accessing the currency value: ${error}.`;
+}
+
+function handleForm(event) {
+  event.preventDefault();
+  const input = document.getElementById("input").value;
+  document.getElementById("input").value = null;
+  getMoney(input);
+  //printBike(response);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleForm);
+});
+
+
+
 
